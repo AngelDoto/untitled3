ce7288362a8b545d50167046fa11ff72a2919da3
AngelDoto <jtonik2013@mail.ru>
2023-09-12 05:45:59 +0300

    Initial commit

---
+++ src/main/java/test1/Cat.java
@@ -0,0 +1,7 @@
+package test1;
+
+public class Cat implements Pet{
+    public void say(){
+        System.out.println("Mew-mew");
+    }
+}
---
+++ src/main/java/test1/Dog.java
@@ -0,0 +1,8 @@
+package test1;
+
+public class Dog implements Pet{
+
+    public void say(){
+        System.out.println("Bow-wow");
+    }
+}
